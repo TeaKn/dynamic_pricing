47ada65b51221372d651ea7b1c03eed0cff78810
TeaKn <ttea.kknezevic@gmail.com>
2022-01-11 09:57:56 +0100

    first commit, spring initialized project

---
+++ price_project/price-project/src/main/java/com/price/PriceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceProjectApplication.class, args);
+	}
+
+}
