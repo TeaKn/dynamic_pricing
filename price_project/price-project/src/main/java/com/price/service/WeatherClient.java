bfe21789127ef2a9961ca8a8035465e0161eb50f
TeaKn <ttea.kknezevic@gmail.com>
2022-01-12 16:23:01 +0100

    adding other classes for webClient but not finished yet

--- price_project/price-project/src/main/java/com/price/service/WeatherClient.java
+++ price_project/price-project/src/main/java/com/price/service/WeatherClient.java
@@ -1,2 +1,15 @@
-package com.price.service;public class WeatherClient {
+package com.price.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherClient {
+
+    private static final String WEATHER_API_BASE_URL = "https://api.srgssr.ch";
+
+
+    public WeatherClient() {
+
+    }
+
 }
