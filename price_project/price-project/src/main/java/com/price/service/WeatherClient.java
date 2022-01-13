9d7f01a0eb83830f9846638a0c527db4901182fc
TeaKn <ttea.kknezevic@gmail.com>
2022-01-13 14:52:57 +0100

    implemented Service, injected with WebClient dep and implemeted getAccesstoken()

--- price_project/price-project/src/main/java/com/price/service/WeatherClient.java
+++ price_project/price-project/src/main/java/com/price/service/WeatherClient.java
@@ -1,15 +1,42 @@
 package com.price.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Service
 public class WeatherClient {
 
-    private static final String WEATHER_API_BASE_URL = "https://api.srgssr.ch";
+    private static final String SRGSSR_API_BASE_URL = "https://api.srgssr.ch/oauth/v1/accesstoken?grant_type=client_credentials";
 
+    private final WebClient webClient;
 
-    public WeatherClient() {
+    // creating an instance of WebClient
 
+    @Autowired
+    public WeatherClient(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder
+                .baseUrl(SRGSSR_API_BASE_URL)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .defaultHeader(HttpHeaders.CONTENT_LENGTH, "0")
+                .defaultHeader(HttpHeaders.CACHE_CONTROL, "no-cache")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Basic " + Base64Utils
+                        .encodeToString(("vhCOppbnnNPlnlonuRqXqIXOzWs9SZKE" + ":" + "R79BNPhRB6IGNN07").getBytes(UTF_8)))
+                .build();
+    }
+
+    // get access token with post request
+    public Mono<String> getAccessToken() {
+        return webClient.get()
+                .uri("")
+                .retrieve()
+                .bodyToMono(String.class);
     }
 
 }
