e9549d44090c28cd70f462999532590b3d4a6843
TeaKn <ttea.kknezevic@gmail.com>
2022-01-13 16:16:47 +0100

    changed default headers to headers in get call instead in WeatherClient constructor

--- price_project/price-project/src/main/java/com/price/service/WeatherClient.java
+++ price_project/price-project/src/main/java/com/price/service/WeatherClient.java
@@ -13,7 +13,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @Service
 public class WeatherClient {
 
-    private static final String SRGSSR_API_BASE_URL = "https://api.srgssr.ch/oauth/v1/accesstoken?grant_type=client_credentials";
+    private static final String SRGSSR_API_BASE_URL = "https://api.srgssr.ch";
 
     private final WebClient webClient;
 
@@ -24,19 +24,25 @@ public class WeatherClient {
         this.webClient = webClientBuilder
                 .baseUrl(SRGSSR_API_BASE_URL)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                .defaultHeader(HttpHeaders.CONTENT_LENGTH, "0")
-                .defaultHeader(HttpHeaders.CACHE_CONTROL, "no-cache")
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "Basic " + Base64Utils
-                        .encodeToString(("vhCOppbnnNPlnlonuRqXqIXOzWs9SZKE" + ":" + "R79BNPhRB6IGNN07").getBytes(UTF_8)))
                 .build();
     }
 
-    // get access token with post request
+    // get access token with get request
     public Mono<String> getAccessToken() {
+        return webClient.get()
+                .uri("/oauth/v1/accesstoken?grant_type=client_credentials")
+                .header(HttpHeaders.CONTENT_LENGTH, "0")
+                .header(HttpHeaders.CACHE_CONTROL, "no-cache")
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + Base64Utils
+                        .encodeToString(("vhCOppbnnNPlnlonuRqXqIXOzWs9SZKE" + ":" + "R79BNPhRB6IGNN07").getBytes(UTF_8)))
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> getGeolocation() {
         return webClient.get()
                 .uri("")
                 .retrieve()
                 .bodyToMono(String.class);
     }
-
 }
