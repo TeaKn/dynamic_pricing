dff9006abbbb1a09434015cd01ade0d8e538e001
TeaKn <ttea.kknezevic@gmail.com>
2022-01-19 16:44:47 +0100

    dela authorization in authentication, dela get request za kraj

--- price_project/price-project/src/main/java/com/price/config/WebClientConfig.java
+++ price_project/price-project/src/main/java/com/price/config/WebClientConfig.java
@@ -1,12 +1,18 @@
 package com.price.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.server.UnAuthenticatedServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -16,13 +22,13 @@ public class WebClientConfig {
 
     private static final String SRGSSR_API_BASE_URL = "https://api.srgssr.ch";
 
-    @Bean
+
+    @Bean()
     WebClient webClient(ReactiveClientRegistrationRepository clientRegistrations) {
 
-        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(
-                clientRegistrations,
-                new UnAuthenticatedServerOAuth2AuthorizedClientRepository()
-        );
+        InMemoryReactiveOAuth2AuthorizedClientService clientService = new InMemoryReactiveOAuth2AuthorizedClientService(clientRegistrations);
+        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager authorizedClientManager = new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistrations, clientService);
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
         oauth.setDefaultClientRegistrationId("srf-meteo");
         return WebClient.builder()
                 .baseUrl(SRGSSR_API_BASE_URL)
