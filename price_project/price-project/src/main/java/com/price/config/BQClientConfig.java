61b33ac81c56508c4402a62936c269d17bd6e871
TeaKn <ttea.kknezevic@gmail.com>
2022-01-24 16:16:10 +0100

    bqclient config added

--- price_project/price-project/src/main/java/com/price/config/BQClientConfig.java
+++ price_project/price-project/src/main/java/com/price/config/BQClientConfig.java
@@ -1,9 +1,14 @@
 package com.price.config;
 
+import com.google.api.gax.paging.Page;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryOptions;
+import io.grpc.Context;
+import io.opencensus.metrics.export.Distribution;
 import org.apache.logging.log4j.spi.LoggerContextKey;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -20,25 +25,47 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+
 @Configuration
 public class BQClientConfig {
+
+    // application credentials provide the required information about the caller making a request to a Google cloud api
+    // presenting application credentials in requests to google cloud apis only identifies the caller as a registered application
+    // if authentication is required the client must also identify the principal running the application, such as a user account or service account
+
+    // to build an application using google cloud apis, follow these general steps:
+    // - choose and use the provided Google cloud client libraries
+    // - determine the correct authentication flow for your application
+    // - pass the application credentials to the client libraries at application startup time, ideally through application default credentials
+
 
     // TODO: IMPLEMENT CLIENT CONFIG CLASS
 
     private static final String GOOGLE_API_BASE_URL = "https://bigquery.googleapis.com";
+    public BigQuery bigQueryInit;
+    //public Storage storage;
 
-    // Initialize client that will be used to send requests. This client only needs to be created
-    // once, and can be reused for multiple requests.
 
-    public BigQuery bigQueryInit() throws IOException {
-        return BigQueryOptions.newBuilder().setProjectId("dhimahi-dynamic-pricing-demo")
+    @Bean // instantiate a new web client
+    public WebClient bqClient() throws IOException {
+
+        // Initialize client that will be used to send requests. This client only needs to be created
+        // once, and can be reused for multiple requests.
+
+        bigQueryInit = BigQueryOptions.newBuilder().setProjectId("dhimahi-dynamic-pricing-demo")
                 .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream(ResourceUtils.getFile("classpath:dhimahi-dynamic-pricing-demo-cb98c398daf8.json"))))
                 .build().getService();
-    }
 
-    public WebClient webClient() {
+        //BigQuery bigqueryInit = BigQueryOptions.getDefaultInstance().getService();
+
         return WebClient.builder()
                 .baseUrl(GOOGLE_API_BASE_URL)
+                //.defaultHeader(HttpHeaders.AUTHORIZATION())
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
                 .build();
     }
 
--- price_project/price-project/src/main/java/com/price/service/BQClient.java
+++ price_project/price-project/src/main/java/com/price/service/BQClient.java
@@ -7,8 +7,10 @@ import org.apache.logging.log4j.spi.LoggerContextKey;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -18,9 +20,12 @@ import java.util.UUID;
 public class BQClient {
 
     @Autowired
-    BQClientConfig bqClientConfig;
-
-    public BQClient(BQClientConfig bqClientConfig) {
+    private final BQClientConfig bqClientConfig;
+    
+    @Autowired
+    private WebClient bqClient;
+    
+    private BQClient(BQClientConfig bqClientConfig) {
         this.bqClientConfig = bqClientConfig;
     }
 
@@ -46,7 +51,7 @@ public class BQClient {
                                 .setUseLegacySql(false).build();
         // Create a job ID so that we can safely retry.
         JobId jobId = JobId.of(UUID.randomUUID().toString());
-        Job queryJob = bqClientConfig.bigQueryInit().create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+        Job queryJob = bqClientConfig.bigQueryInit.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
 
         // Wait for the query to complete.
         try {
@@ -76,11 +81,112 @@ public class BQClient {
         return "some Error occured with the query";
     }
 
-    public com.google.api.services.bigquery.model.Model getArimaModel() {
-        // TODO: IMPLEMENT GET ARIMA MODEL
+    public Mono<String> getArimaModel(String projectId, String datasetId, String modelId) {
+        // TODO: IMPLEMENT GET ARIMA MODEL, ne radi 401
         // GET https://bigquery.googleapis.com/bigquery/v2/projects/{projectId}/datasets/{datasetId}/models/{modelId}
+        return bqClient.get()
+                .uri("/bigquery/v2/projects/{projectId}/datasets/{datasetId}/models/{modelId}", projectId, datasetId, modelId)
+                .retrieve()
+                .bodyToMono(String.class);
+
+    }
+
+    public String ArimaModel() throws IOException {
+
+        String query = """
+                #standardSQL
+                CREATE OR REPLACE MODEL bqml_tutorials.nyc_citibike_arima_model_group
+                OPTIONS
+                  (model_type = 'ARIMA_PLUS',
+                   time_series_timestamp_col = 'date',
+                   time_series_data_col = 'num_trips',
+                   time_series_id_col = 'start_station_name',
+                   auto_arima_max_order = 5
+                  ) AS
+                SELECT
+                   start_station_name,
+                   EXTRACT(DATE from starttime) AS date,
+                   COUNT(*) AS num_trips
+                FROM
+                  `bigquery-public-data`.new_york.citibike_trips
+                WHERE start_station_name LIKE '%Central Park%'
+                GROUP BY start_station_name, date
+                """;
+
+        QueryJobConfiguration queryConfig =
+                QueryJobConfiguration.newBuilder(query)
+                        .setUseLegacySql(false).build();
+
+        // Create a job ID so that we can safely retry.
+        JobId jobId = JobId.of(UUID.randomUUID().toString());
+        Job queryJob = bqClientConfig.bigQueryInit.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+
+        // Wait for the query to complete.
+        try {
+            queryJob = queryJob.waitFor();
+            // Check for errors
+            if (queryJob == null) {
+                throw new RuntimeException("Job no longer exists");
+            } else if (queryJob.getStatus().getError() != null) {
+                // You can also look at queryJob.getStatus().getExecutionErrors() for all
+                // errors, not just the latest one.
+                throw new RuntimeException(queryJob.getStatus().getError().toString());
+            }
+            // Get the results.
+            TableResult result = queryJob.getQueryResults();
+            Flux<String> returnValue = new Flux<String>() {
+                @Override
+                public void subscribe(CoreSubscriber<? super String> coreSubscriber) {
+
+                }
+            };
+            return result.toString();
+
+        } catch (BigQueryException | InterruptedException e) {
+            System.out.println("Query not performed \n" + e);
+
+        }
+        return "some Error occured with the query";
+    }
 
 
-        return new com.google.api.services.bigquery.model.Model();
+    public String explainForecast() throws IOException {
+
+        String query = "SELECT * FROM ML.EXPLAIN_FORECAST(MODEL bqml_tutorials.nyc_citibike_arima_model, STRUCT(365 AS horizon, 0.9 AS confidence_level))";
+
+        QueryJobConfiguration queryConfig =
+                QueryJobConfiguration.newBuilder(query)
+                        .setUseLegacySql(false).build();
+
+        // Create a job ID so that we can safely retry.
+        JobId jobId = JobId.of(UUID.randomUUID().toString());
+        Job queryJob = bqClientConfig.bigQueryInit.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+
+        // Wait for the query to complete.
+        try {
+            queryJob = queryJob.waitFor();
+            // Check for errors
+            if (queryJob == null) {
+                throw new RuntimeException("Job no longer exists");
+            } else if (queryJob.getStatus().getError() != null) {
+                // You can also look at queryJob.getStatus().getExecutionErrors() for all
+                // errors, not just the latest one.
+                throw new RuntimeException(queryJob.getStatus().getError().toString());
+            }
+            // Get the results.
+            TableResult result = queryJob.getQueryResults();
+            Flux<String> returnValue = new Flux<String>() {
+                @Override
+                public void subscribe(CoreSubscriber<? super String> coreSubscriber) {
+
+                }
+            };
+            return result.toString();
+
+        } catch (BigQueryException | InterruptedException e) {
+            System.out.println("Query not performed \n" + e);
+
+        }
+        return "some Error occured with the query";
     }
 }
