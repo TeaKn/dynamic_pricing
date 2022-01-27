5647a7db5fd6b658eef58caee45a85ec3a2ef9ea
TeaKn <ttea.kknezevic@gmail.com>
2022-01-27 21:55:08 +0100

    implemented tickter service interfac

---
+++ price_project/price-project/src/main/java/com/price/service/TicketService.java
@@ -0,0 +1,11 @@
+package com.price.service;
+
+import com.price.shared.dto.TicketDTO;
+
+// TODO: FIND DATABASE BACKED CORE INTERFACE SPRING TO MAKE THIS WORK
+
+public interface TicketService {
+
+    TicketDTO createTicket(TicketDTO ticketDTO);
+
+}
