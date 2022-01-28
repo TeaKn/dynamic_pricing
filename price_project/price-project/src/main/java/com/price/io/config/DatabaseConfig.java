19fbc5678cd83e1dcb7e7d25439b3327d313a306
TeaKn <ttea.kknezevic@gmail.com>
2022-01-28 14:46:17 +0100

    database config for r2dbc repo

--- price_project/price-project/src/main/java/com/price/io/config/DatabaseConfig.java
+++ price_project/price-project/src/main/java/com/price/io/config/DatabaseConfig.java
@@ -1,2 +1,18 @@
-package com.price.io.config;public class DatabaseConfig {
+package com.price.io.config;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+@Configuration
+@EnableR2dbcRepositories
+public class DatabaseConfig extends AbstractR2dbcConfiguration {
+
+    @Override
+    public ConnectionFactory connectionFactory(){
+        return ConnectionFactories.get("r2dbc:postgresql://localhost:5432/dyn_price");
+    }
+
 }
