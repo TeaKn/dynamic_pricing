180f17015c3b1fd41e49eff511c0b185e280be8b
TeaKn <ttea.kknezevic@gmail.com>
2022-01-28 13:43:54 +0100

    implemented venue entity and venue repository, db layer

--- price_project/price-project/src/main/java/com/price/io/entity/VenueEntity.java
+++ price_project/price-project/src/main/java/com/price/io/entity/VenueEntity.java
@@ -1,2 +1,41 @@
-package com.price.io.entity;public class VenueEntity {
+package com.price.io.entity;
+
+public class VenueEntity {
+
+    private final Integer venue_id;
+    private Double price_max;
+    private Double price_min;
+    private Double adult_base_price;
+
+    public VenueEntity(Integer venue_id) {
+        this.venue_id = venue_id;
+    }
+
+    public Integer getVenue_id() {
+        return venue_id;
+    }
+
+    public Double getPrice_max() {
+        return price_max;
+    }
+
+    public void setPrice_max(Double price_max) {
+        this.price_max = price_max;
+    }
+
+    public Double getPrice_min() {
+        return price_min;
+    }
+
+    public void setPrice_min(Double price_min) {
+        this.price_min = price_min;
+    }
+
+    public Double getAdult_base_price() {
+        return adult_base_price;
+    }
+
+    public void setAdult_base_price(Double adult_base_price) {
+        this.adult_base_price = adult_base_price;
+    }
 }
--- price_project/price-project/src/main/java/com/price/io/repositories/VenueRepository.java
+++ price_project/price-project/src/main/java/com/price/io/repositories/VenueRepository.java
@@ -1,2 +1,14 @@
-package com.price.io.repositories;public class VenueRepository {
+package com.price.io.repositories;
+
+
+import com.price.io.entity.VenueEntity;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface VenueRepository extends ReactiveCrudRepository<VenueEntity, Integer> {
+
+    @Query("SELECT * FROM venueEntity WHERE venue_id = :venue_id")
+    Mono<VenueEntity> findByVenueId(Integer venue_id);
+
 }
