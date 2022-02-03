99624965aa60a0a988cecd88706567ff66fadf2b
TeaKn <ttea.kknezevic@gmail.com>
2022-02-03 17:08:15 +0100

    added @Param to query

--- price_project/price-project/src/main/java/com/price/io/repositories/WeatherRepository.java
+++ price_project/price-project/src/main/java/com/price/io/repositories/WeatherRepository.java
@@ -2,12 +2,13 @@ package com.price.io.repositories;
 
 import com.price.io.entity.WeatherEntity;
 import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
 public interface WeatherRepository extends ReactiveCrudRepository<WeatherEntity, String> {
 
-    @Query("SELECT * FROM weatherEntity WHERE date = :date")
-    Flux<WeatherEntity> findByDate(String date);
+    @Query("SELECT * FROM weather WHERE date = :date")
+    Flux<WeatherEntity> findByDate(@Param("date") String date);
 
 }
