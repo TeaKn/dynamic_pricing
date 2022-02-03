bd63884f703c165e70b53b885fb5c6c6a59262d9
TeaKn <ttea.kknezevic@gmail.com>
2022-02-03 17:07:18 +0100

    addded reactor debug option

--- price_project/price-project/src/main/java/com/price/PriceProjectApplication.java
+++ price_project/price-project/src/main/java/com/price/PriceProjectApplication.java
@@ -3,12 +3,14 @@ package com.price;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @PropertySource("classpath:application.yml")
 public class PriceProjectApplication {
 
 	public static void main(String[] args) {
+		Hooks.onOperatorDebug();
 		SpringApplication.run(PriceProjectApplication.class, args);
 	}
 
